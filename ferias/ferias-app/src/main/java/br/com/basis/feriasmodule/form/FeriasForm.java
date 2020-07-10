50404e6c832a6e1151389d3e6abc5cb69f71ca2d
Jean Pandolfi <jean.pandolfi@basis.com.br>
2020-07-10 12:58:46 -0300

    Form de requerimento de tela completo

--- ferias/ferias-app/src/main/java/br/com/basis/feriasmodule/form/FeriasForm.java
+++ ferias/ferias-app/src/main/java/br/com/basis/feriasmodule/form/FeriasForm.java
@@ -1,5 +1,6 @@
 package br.com.basis.feriasmodule.form;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.function.Predicate;
 import javax.annotation.Nonnull;
@@ -9,54 +10,108 @@ import org.opensingular.form.SInfoType;
 import org.opensingular.form.SInstance;
 import org.opensingular.form.STypeComposite;
 import org.opensingular.form.TypeBuilder;
+import org.opensingular.form.type.core.SIInteger;
 import org.opensingular.form.type.core.STypeBoolean;
+import org.opensingular.form.type.core.STypeInteger;
 import org.opensingular.form.type.core.STypeString;
+import org.opensingular.form.type.util.SIYearMonth;
+import org.opensingular.form.type.util.STypeYearMonth;
+import org.opensingular.form.validation.InstanceValidatable;
 import org.opensingular.form.view.SViewByBlock;
 
 import static org.opensingular.form.util.SingularPredicates.typeValueIsTrueAndNotNull;
 
 @SInfoType(spackage = FeriasPackage.class, name = "Ferias")
 public class FeriasForm extends STypeComposite<SIComposite> {
+	
+	public STypeYearMonth inicio;
+	public STypeYearMonth fim;
+	public STypeInteger dias;
+	public STypeInteger diasVendidos;
+    public STypeBoolean isVenderDias;
+    public STypeBoolean antecipar13;
+    
 
-    public STypeBoolean radioBox;
-    public STypeString descriptionTextArea;
-    public STypeString typeSelection;
 
     @Override
     protected void onLoadType(@Nonnull TypeBuilder tb) {
         asAtr().displayString("Nova Solicitação");
-
-        radioBox = addFieldBoolean("radioBox");
-        radioBox.withRadioView("Sim", "Não")
-            .asAtr().required().label("Novo Usuário?").asAtrAnnotation().setAnnotated()
+        
+        inicio = addField("inicio", STypeYearMonth.class);
+        inicio
+	        .asAtr().label("Início do período aquisitivo").required()
+	        .asAtrAnnotation().setAnnotated()
+	        .asAtrBootstrap().colPreference(2);
+        
+        fim = addField("fim", STypeYearMonth.class);
+        fim
+	        .asAtr().label("Fim do período aquisitivo").required()
+	        .asAtrAnnotation().setAnnotated()
+	        .asAtrBootstrap().colPreference(2);		
+        		
+        dias = this.addFieldInteger("dias", true);
+        dias.asAtr().label("Quantidade de dias")
+	        .asAtrAnnotation().setAnnotated()
+	        .asAtrBootstrap().colPreference(2);
+        
+        isVenderDias = addFieldBoolean("isVenderDias", true);
+        isVenderDias.withRadioView("Sim", "Não")
+            .asAtr().label("Vender dias?").asAtrAnnotation().setAnnotated()
             .asAtrBootstrap().colPreference(2);
-
-        typeSelection = addFieldString("typeSelection");
-        typeSelection.selectionOf(Profissoes.valuesOfDescricao()).asAtr().label("Profissão").required(true);
-        typeSelection.asAtr().dependsOn(radioBox).exists(isNewUser()).asAtrAnnotation().setAnnotated()
-            .asAtrBootstrap().colPreference(3);
-
-        descriptionTextArea = addFieldString("descriptionTextArea");
-        descriptionTextArea
-            .asAtr().required().label("Descrição")
-            .asAtrBootstrap().colPreference(12);
-        descriptionTextArea
-            .withTextAreaView(sViewTextArea -> sViewTextArea.setLines(4))
-            .asAtr().maxLength(4000).asAtrAnnotation().setAnnotated();
-
-
+        
+        diasVendidos = this.addFieldInteger("diasVendidos", true);
+        diasVendidos.asAtr().label("Dias vendidos").dependsOn(isVenderDias).exists(this.isVenderDias())
+			.asAtrAnnotation().setAnnotated()
+			.asAtrBootstrap().colPreference(2);
+        
+                
+        this.antecipar13 = this.addFieldBoolean("antecipar13", true);
+        this.antecipar13.withRadioView("Sim", "Não")
+        				.asAtr().label("Antecipar décimo terceiro?")
+				        .asAtrAnnotation().setAnnotated()
+						.asAtrBootstrap().colPreference(2);
+        
+        this.addInstanceValidator(this::verificaNumeroDeDias);
+        this.addInstanceValidator(this::verificaDatas);
+        
         this.withView(new SViewByBlock(), block ->
-            block.newBlock("Requerimento")
-                .add(radioBox)
-                .add(typeSelection)
-                .add(descriptionTextArea)
+            block.newBlock("Solicitação de férias")
+            	.add(inicio)
+            	.add(fim)
+            	.add(dias)
+                .add(isVenderDias)
+                .add(diasVendidos)
+                .add(antecipar13)
             );
     }
 
-    private Predicate<SInstance> isNewUser() {
-        return typeValueIsTrueAndNotNull(radioBox);
+    private Predicate<SInstance> isVenderDias() {
+        return typeValueIsTrueAndNotNull(isVenderDias);
     }
-
+    
+    private void verificaNumeroDeDias(InstanceValidatable<SIComposite> validator) {
+        SIComposite myForm = validator.getInstance();
+ 
+        int diasFerias = myForm.findNearestOrException(dias).getInteger();
+        int diasVender = myForm.findNearestOrException(diasVendidos).exists() ? 
+        				myForm.findNearestOrException(diasVendidos).getInteger() : 0;		
+        
+        if ((diasFerias + diasVender) > 30) {
+            validator.error("A soma dos dias de férias com os dias vendidos não pode ser maior que 30");
+        }
+    }
+   
+    private void verificaDatas(InstanceValidatable<SIComposite> validator) {
+        SIComposite myForm = validator.getInstance();
+ 
+        SIYearMonth mesInicio = myForm.findNearestOrException(inicio);
+        SIYearMonth mesFim = myForm.findNearestOrException(fim);
+ 
+        if (mesInicio.isAfter(mesFim)) {
+            validator.error("O início do peŕiodo aquisitivo não pode ser menor que o fim do período aquisitivo");
+        }
+    }
+    
     public enum Profissoes {
 
         DESENVOLVEDOR("Desenvolvedor"),
